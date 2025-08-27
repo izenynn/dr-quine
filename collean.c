dd5b0435730a823fe99c9a431355882c88190237
izenynn <me@izenynn.com>
2025-08-27 15:52:05 +0200

    fix: clang format

--- collean.c
+++ collean.c
@@ -20,7 +20,7 @@ int main(void)
 	 */
 
 	const char *s =
-		"/*%3$c * Outside comment%3$c */%3$c%3$c#include <stdio.h>%3$c%3$c#define TAB 9%3$c#define NL 10%3$c#define DQUOTE 34%3$c%3$cstatic void foo(void)%3$c{%3$c%2$c(void)0;%3$c}%3$c%3$cint main(void)%3$c{%3$c%2$c/*%3$c%2$c * Inside comment%3$c%2$c */%3$c%3$c%2$cconst char* s =%3$c%2$c%2$c%4$c%1$s%4$c;%3$c%3$c%2$cfoo();%3$c%2$cprintf(s, s, TAB, NL, DQUOTE);%3$c%3$c%2$creturn 0;%3$c}%3$c";
+		"/*%3$c * Outside comment%3$c */%3$c%3$c#include <stdio.h>%3$c%3$c#define TAB 9%3$c#define NL 10%3$c#define DQUOTE 34%3$c%3$cstatic void foo(void)%3$c{%3$c%2$c(void)0;%3$c}%3$c%3$cint main(void)%3$c{%3$c%2$c/*%3$c%2$c * Inside comment%3$c%2$c */%3$c%3$c%2$cconst char *s =%3$c%2$c%2$c%4$c%1$s%4$c;%3$c%3$c%2$cfoo();%3$c%2$cprintf(s, s, TAB, NL, DQUOTE);%3$c%3$c%2$creturn 0;%3$c}%3$c";
 
 	foo();
 	printf(s, s, TAB, NL, DQUOTE);
