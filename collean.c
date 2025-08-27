a6744ec8d33825423873b99f5dfa9fcf98074f8b
izenynn <me@izenynn.com>
2025-08-27 16:58:21 +0200

    refactor: remove collean macros

--- collean.c
+++ collean.c
@@ -4,10 +4,6 @@
 
 #include <stdio.h>
 
-#define TAB 9
-#define NL 10
-#define DQUOTE 34
-
 static void foo(void)
 {
 	(void)0;
@@ -20,10 +16,10 @@ int main(void)
 	 */
 
 	const char *s =
-		"/*%3$c * Outside comment%3$c */%3$c%3$c#include <stdio.h>%3$c%3$c#define TAB 9%3$c#define NL 10%3$c#define DQUOTE 34%3$c%3$cstatic void foo(void)%3$c{%3$c%2$c(void)0;%3$c}%3$c%3$cint main(void)%3$c{%3$c%2$c/*%3$c%2$c * Inside comment%3$c%2$c */%3$c%3$c%2$cconst char *s =%3$c%2$c%2$c%4$c%1$s%4$c;%3$c%3$c%2$cfoo();%3$c%2$cprintf(s, s, TAB, NL, DQUOTE);%3$c%3$c%2$creturn 0;%3$c}%3$c";
+		"/*%3$c * Outside comment%3$c */%3$c%3$c#include <stdio.h>%3$c%3$cstatic void foo(void)%3$c{%3$c%2$c(void)0;%3$c}%3$c%3$cint main(void)%3$c{%3$c%2$c/*%3$c%2$c * Inside comment%3$c%2$c */%3$c%3$c%2$cconst char *s =%3$c%2$c%2$c%4$c%1$s%4$c;%3$c%3$c%2$cfoo();%3$c%2$cprintf(s, s, 9, 10, 34);%3$c%3$c%2$creturn 0;%3$c}%3$c";
 
 	foo();
-	printf(s, s, TAB, NL, DQUOTE);
+	printf(s, s, 9, 10, 34);
 
 	return 0;
 }
