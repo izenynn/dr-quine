71efd9c6202650aabd27845d7b8ff3b750a729cc
izenynn <me@izenynn.com>
2025-08-27 17:34:23 +0200

    feat: add tab char to grace for consistency across executables

--- grace.c
+++ grace.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 
 #define F "grace_kid.c"
-#define S "/*%2$c * Outside comment%2$c */%2$c%2$c#include <stdio.h>%2$c%2$c#define F %3$cgrace_kid.c%3$c%2$c#define S %3$c%1$s%3$c%2$c#define MAIN() int main(void) { const char *s = S; FILE *f = fopen(F, %3$cw%3$c); if (f == NULL) return 1; fprintf(f, s, s, 10, 34); fclose(f); return 0; }%2$c%2$cMAIN()%2$c"
-#define MAIN() int main(void) { const char *s = S; FILE *f = fopen(F, "w"); if (f == NULL) return 1; fprintf(f, s, s, 10, 34); fclose(f); return 0; }
+#define S "/*%3$c * Outside comment%3$c */%3$c%3$c#include <stdio.h>%3$c%3$c#define F %4$cgrace_kid.c%4$c%3$c#define S %4$c%1$s%4$c%3$c#define MAIN() int main(void) { const char *s = S; FILE *f = fopen(F, %4$cw%4$c); if (f == NULL) return 1; fprintf(f, s, s, 9, 10, 34); fclose(f); return 0; }%3$c%3$cMAIN()%3$c"
+#define MAIN() int main(void) { const char *s = S; FILE *f = fopen(F, "w"); if (f == NULL) return 1; fprintf(f, s, s, 9, 10, 34); fclose(f); return 0; }
 
 MAIN()
